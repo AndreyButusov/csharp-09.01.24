b08b7e5e320dfc547e294293de86e598066c2a5e
Andrey Butusov <agbutusov@mail.ru>
2024-01-10 00:03:08 +0300

    add lesson2:task2-4

---
+++ Task3/Program.cs
@@ -0,0 +1,12 @@
+﻿//int n = 10;
+int[] array = { 12, 51, 41, 37, 18, 35, 11, 32, 82, 51 };
+int i = 0;
+
+while (i < array.Length)
+{
+    if (array[i] % 2 == 0)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    i = i + 1;
+}
